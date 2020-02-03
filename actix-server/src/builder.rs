7ac4f2d8824f3cac8b30676cb5aabdeaa4bce872
Rudolf B <5439317+Rudi3@users.noreply.github.com>
2020-02-03 22:36:24 +0100

    test

--- actix-server/src/builder.rs
+++ actix-server/src/builder.rs
@@ -447,7 +447,7 @@ impl ServerBuilder {
     fn close_socks(&mut self) {
         println!("test");
         let sockets_t = mem::replace(&mut self.sockets, Vec::new());
-        println!("{}", sockets_t.len());
+        println!("{}", self.sockets.len());
         for socket_t in sockets_t {
             let socket = socket_t.2;
             match socket {
